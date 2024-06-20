0e7ea0d08628a0a165d466782c9d3960d288239f
NiteshMourya009 <niteshmourya009@gmail.com>
2024-06-20 12:28:04 +0530

    Leetcode Problems

--- src/Dyanmic_Programming/memoization/minPathSum.java
+++ src/Dyanmic_Programming/memoization/minPathSum.java
@@ -1,23 +1,30 @@
 package Dyanmic_Programming.memoization;
 
 public class minPathSum {
-    public  static int minSUm(int i, int j, int[][]arr){
-        if(i==0 && j==0){
-            return arr[i][j] ;
-        }
+    public  static int minSUm(int i, int j, int[][]arr,int[][]dp){
+
         if(i<0 || j<0){
             return Integer.MAX_VALUE ;
         }
-        int up= arr[i][j] + minSUm(i-1,j, arr) ;
-        int right= arr[i][j] + minSUm(i, j-1, arr) ;
-
-        return Math.min(up, right) ;
+        if(i==0 && j==0){
+            return dp[i][j] = arr[i][j] ;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j] ;
+        }
+        return dp[i][j]=arr[i][j] +Math.min(minSUm(i-1,j,arr,dp),minSUm(i,j-1,arr,dp)) ;
     }
     public static void main(String[] args) {
         int[][] grid= {{1,3,1},{1,5,1},{4,2,1}} ;
         int m=grid.length ;
         int n= grid[0].length ;
-        System.out.println(minSUm(m-1,n-1, grid));
+        int[][]dp =new int[m][n] ;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                dp[i][j] =-1 ;
+            }
+        }
+        System.out.println(minSUm(m-1,n-1, grid,dp));
 
     }
 }
