30683097b601d7bcb31bb8492185b2a608d79962
NiteshMourya009 <niteshmourya009@gmail.com>
2024-06-30 11:28:27 +0530

    Problem Added

---
+++ src/com/LeetCode_Question/Lemonade_Change.java
@@ -0,0 +1,42 @@
+package com.LeetCode_Question;
+
+public class Lemonade_Change {
+    public static boolean lemonadeChange(int[] bills) {
+        int five=0 ;
+        int ten=0 ;
+
+        for(int i=0; i<bills.length ; i++){
+            if(bills[i]==5){
+                five++ ;
+            }
+            else if(bills[i]==10){
+                if(five>0){
+                    five-- ;
+                    ten++ ;
+
+                }
+                else{
+                    return false ;
+                }
+            }
+            else{
+                if(five>0 && ten>0){
+                    five-- ;
+                    ten-- ;
+                }
+                else if(five>=3){
+                    five= five-3 ;
+                }
+                else
+                    return false ;
+
+            }
+
+        }
+        return true ;
+    }
+    public static void main(String[] args) {
+        int[]nums= {5,5,5,10,20} ;
+        System.out.println(lemonadeChange(nums));
+    }
+}
